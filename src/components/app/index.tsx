b5790c49002195fb0c993f499850e37f0ea3f310
Kuzin Artemiy <kuzin_artemiy@list.ru>
2023-04-23 00:30:04 +0500

    feat: add text component && refactor css colors

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -2,28 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import styles from './app.module.scss';
 import Footer from '../footer/footer';
+import Text from '../text';
 
 function App() {
   return (
     <>
-    <div className={styles.app}>
-      <header className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt="logo" />
-        <p className={`text_type_h1 color_gradient ${styles.text}`}>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={styles.appLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div className={`color_gradient ${styles.block}`}></div>
-      </header>
-    </div>
-    <Footer />
+      <div className={styles.app}>
+        <header className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className={styles.appLink}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <Footer />
     </>
   );
 }
