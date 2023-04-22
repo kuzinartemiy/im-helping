b6c89d78b253351925b2f3518e57fbfdfae76322
Kuzin Artemiy <kuzin_artemiy@list.ru>
2023-04-23 00:30:27 +0500

    fix: remove text import

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import styles from './app.module.scss';
 import Footer from '../footer/footer';
-import Text from '../text';
 
 function App() {
   return (
