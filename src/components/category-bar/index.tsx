3bd18e781f35d988c9829dc189dfbcb9b0a48ebf
Kuzin Artemiy <kuzin_artemiy@list.ru>
2023-04-22 23:38:03 +0500

    fix: remove fragment & change bdColor name

--- src/components/category-bar/index.tsx
+++ src/components/category-bar/index.tsx
@@ -3,20 +3,18 @@ import styles from './category-bar.module.scss';
 
 interface ICategoryBarProps {
   children?: React.ReactNode
-  bcColor: string
+  bgColor: string
 }
 
-const CategoryBar = ({ children, bcColor = '#818C99' }: ICategoryBarProps): JSX.Element => (
-  <>
-    <div className={styles.categoryBar}>
-      <svg fill="none" viewBox="0 0 216 25" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0 0L215.091 7L197.273 25H0V0Z" fill={bcColor}/>
-      </svg>
-      <div className={styles.content}>
-        {children}
-      </div>
+const CategoryBar = ({ children, bgColor = '#818C99' }: ICategoryBarProps) => (
+  <div className={styles.categoryBar}>
+    <svg fill="none" viewBox="0 0 216 25" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 0L215.091 7L197.273 25H0V0Z" fill={bgColor}/>
+    </svg>
+    <div className={styles.content}>
+      {children}
     </div>
-  </>
+  </div>
 );
 
 export default CategoryBar;
