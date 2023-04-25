a4e82bbb9fed251cc7adab78e33e839629813c65
Avavery <juliya.belka.19@yandex.ru>
2023-04-25 11:45:10 +0300

    fix reprimands

--- src/components/circle-button/index.tsx
+++ src/components/circle-button/index.tsx
@@ -1,37 +1,35 @@
 import styles from './circle-button.module.scss';
 import { useEffect, useState } from 'react';
 
-interface ICircleButtonProps {
-  isPrimary?: boolean
+interface ICircleButtonProps extends React.PropsWithChildren<Omit<React.HTMLProps<HTMLButtonElement>, 'type' | 'size'>> {
   backgroundColor?: string
-  backgroundColorHover?: string
   backgroundColorPressed: string
-  disabled?: boolean
   children?: React.ReactNode
   onClick: () => void
+  size: 'small' | 'large'
 }
 
-const CircleButton: React.FC<ICircleButtonProps> = ({ isPrimary, backgroundColor, backgroundColorHover, disabled, children, onClick, backgroundColorPressed }) => {
-  const [hover, setHover] = useState<string>(backgroundColor ?? 'var(--color-primary)');
+const CircleButton: React.FC<ICircleButtonProps> = ({ backgroundColor, children, onClick, backgroundColorPressed, size = 'small', ...props }) => {
+  const [pressed, setPressed] = useState<string>(backgroundColor ?? 'var(--color-primary)');
   const [isActive, setIsActive] = useState<boolean>(false);
 
   useEffect(() => {
     if (isActive) {
-      setHover(backgroundColorPressed ?? 'var(--bg-button-wave)');
+      setPressed(backgroundColorPressed ?? 'var(--bg-button-wave)');
+    } else {
+      setPressed(backgroundColor ?? 'var(--color-primary)');
     }
   });
 
   return (
     <button
-      className={`${styles.button} ${isPrimary !== undefined ? styles.button__primary : styles.button__secondary}`}
-      style={{ backgroundColor: !(disabled ?? false) ? hover : 'var(--bg-button-disabled)' }}
-      disabled={disabled ?? false}
-      onMouseOver={() => { setHover(backgroundColorHover ?? 'var(--bg-button-wave)'); }}
-      onMouseLeave={() => { setHover(backgroundColor ?? 'var(--color-primary)'); }}
+      className={`${styles.button} ${styles[`button__${size}`]}`}
+      style={{ backgroundColor: pressed }}
       onClick={() => {
         onClick();
         setIsActive(!isActive);
       }}
+      {...props}
     >
       {children}
     </button>
