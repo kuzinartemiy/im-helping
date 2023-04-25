02e13086607f3609045d2e0420f73ba83e4ef960
Avavery <juliya.belka.19@yandex.ru>
2023-04-25 12:57:51 +0300

    fix reprimands

--- src/components/circle-button/index.tsx
+++ src/components/circle-button/index.tsx
@@ -1,34 +1,16 @@
 import styles from './circle-button.module.scss';
-import { useEffect, useState } from 'react';
 
 interface ICircleButtonProps extends React.PropsWithChildren<Omit<React.HTMLProps<HTMLButtonElement>, 'type' | 'size'>> {
-  backgroundColor?: string
-  backgroundColorPressed: string
   children?: React.ReactNode
   onClick: () => void
   size: 'small' | 'large'
 }
 
-const CircleButton: React.FC<ICircleButtonProps> = ({ backgroundColor, children, onClick, backgroundColorPressed, size = 'small', ...props }) => {
-  const [pressed, setPressed] = useState<string>(backgroundColor ?? 'var(--color-primary)');
-  const [isActive, setIsActive] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (isActive) {
-      setPressed(backgroundColorPressed ?? 'var(--bg-button-wave)');
-    } else {
-      setPressed(backgroundColor ?? 'var(--color-primary)');
-    }
-  });
-
+const CircleButton: React.FC<ICircleButtonProps> = ({ children, onClick, size = 'small', ...props }) => {
   return (
     <button
       className={`${styles.button} ${styles[`button__${size}`]}`}
-      style={{ backgroundColor: pressed }}
-      onClick={() => {
-        onClick();
-        setIsActive(!isActive);
-      }}
+      onClick={onClick}
       {...props}
     >
       {children}
