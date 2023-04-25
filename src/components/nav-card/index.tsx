2c2b72426e93d80c060d152dc9c7cfe51fe61180
Luba-web <ldvasileva87@yandex.ru>
2023-04-25 21:05:43 +0300

    fix: add nav-cards.constants

--- src/components/nav-card/index.tsx
+++ src/components/nav-card/index.tsx
@@ -1,43 +1,14 @@
 import { Link } from 'react-router-dom';
 import styles from './nav-card.module.scss';
-
-import { ReactComponent as IconMaps } from '../../assets/icons/application-card.svg';
-import { ReactComponent as IconActiveList } from '../../assets/icons/active-applications.svg';
-import { ReactComponent as IconCompleted } from '../../assets/icons/completed-applications.svg';
-
 import Text from '../text';
-
-interface ICard {
-  text: string
-  icon: React.ReactNode
-  path: string
-  index?: number
-}
-
-// массив в завимости от выбраной роли
-export const data: ICard[] = [{
-  text: 'Карта заявок',
-  icon: <IconMaps className={styles.colorIcon}/>,
-  path: '/',
-}, {
-  text: 'Активные заявки',
-  icon: <IconActiveList className={styles.colorIcon}/>,
-  path: '/',
-}, {
-  text: 'Завершенные заявки',
-  icon: <IconCompleted className={styles.colorIcon}/>,
-  path: '/',
-},
-];
+import { ICard } from '../nav-cards/nav-cards.constants';
 
 function NavCard(props: ICard) {
   return (
-    <li key={props.index} className={styles.linkCard}>
-      <Link to={props.path} className={styles.link}>
-        {props.icon}
-        <Text tag = 'p' weight = '400' size = '16' color = 'white'>{props.text}</Text>
-      </Link>
-    </li>
+    <Link to={props.path} className={styles.link}>
+      {props.icon}
+      <Text tag = 'p' weight = '400' size = '16' color = 'white'>{props.text}</Text>
+    </Link>
   );
 }
 
--- src/components/nav-cards/index.tsx
+++ src/components/nav-cards/index.tsx
@@ -1,12 +1,14 @@
 import styles from './nav-cards.module.scss';
-
-import NavCard, { data } from '../nav-card';
+import NavCard from '../nav-card';
+import { data } from './nav-cards.constants';
 
 function NavCards() {
   return (
     <ul className={styles.linkCardContainer}>
       {data.map((item, index) => {
-        return <NavCard text={item.text} icon={item.icon} key={index} path={item.path}/>;
+        return <li key={index} className={styles.linkCard}>
+          <NavCard text={item.text} icon={item.icon} path={item.path}/>
+        </li>
       })}
     </ul>
   );
