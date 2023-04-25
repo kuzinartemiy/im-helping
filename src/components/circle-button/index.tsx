c29a503dd6c9b3bf1add37827ab7ea3f4eb02ae9
Avavery <juliya.belka.19@yandex.ru>
2023-04-25 20:05:15 +0300

    fix reprimands v.2

--- src/components/circle-button/index.tsx
+++ src/components/circle-button/index.tsx
@@ -2,15 +2,14 @@ import styles from './circle-button.module.scss';
 
 interface ICircleButtonProps extends React.PropsWithChildren<Omit<React.HTMLProps<HTMLButtonElement>, 'type' | 'size'>> {
   children?: React.ReactNode
-  onClick: () => void
-  size: 'small' | 'large'
+  onClick?: () => void
+  size?: 'small' | 'medium' | 'large'
 }
 
-const CircleButton: React.FC<ICircleButtonProps> = ({ children, onClick, size = 'small', ...props }) => {
+const CircleButton: React.FC<ICircleButtonProps> = ({ children, size = 'small', ...props }) => {
   return (
     <button
       className={`${styles.button} ${styles[`button__${size}`]}`}
-      onClick={onClick}
       {...props}
     >
       {children}
