8d168487184ecb3d70f8b00eaf1ce5779cff5b73
Zhuk Ivan <zhukekIvan@yandex.ru>
2023-04-25 22:07:54 +0300

    radio buttons and radius ready

---
+++ src/components/radio-button/index.tsx
@@ -0,0 +1,23 @@
+import styles from './radio-button.module.scss';
+import type { ChangeEventHandler, FC } from 'react';
+
+interface IRadioButton {
+  name: string
+  value: number
+  text: string | number
+  onChange: ChangeEventHandler<HTMLInputElement>
+  actualValue: string | undefined
+}
+
+const RadioButton: FC<IRadioButton> = ({ name, value, text, onChange, actualValue }) => {
+  return (
+    <div className={`${styles.radioButton} ${`${value}` === actualValue ? styles.radioButton_selected : ''}`}>
+      <label className={styles.radioLabel}>
+        {text}
+        <input type='radio' name={name} className={styles.radioInput} value={value} onChange={onChange}/>
+      </label>
+    </div>
+  );
+};
+
+export default RadioButton;
---
+++ src/components/radius-search/index.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import type { ChangeEventHandler } from 'react';
+import RadioButton from '../radio-button';
+import styles from './radius-search.module.scss';
+
+const RadiusSearch = () => {
+  const [value, setValue] = useState<string | undefined>('1');
+
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div className={styles.radiusSearch}>
+      <RadioButton name='radius' value={1} text={'1 км'} onChange={onChange} actualValue={value}/>
+      <RadioButton name='radius' value={3} text={'3 км'} onChange={onChange} actualValue={value}/>
+      <RadioButton name='radius' value={5} text={'5 км'} onChange={onChange} actualValue={value}/>
+    </div>
+  );
+};
+
+export default RadiusSearch;
