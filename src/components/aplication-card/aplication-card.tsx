6f94038ffd136e4261f93c4c12c24eccf5bf5dc8
Kamil-Kalandarov <iam@kalandarov-13.ru>
2023-04-26 12:54:04 +0300

    исправил все ошибки, указанные в комментах

--- src/components/aplication-card/aplication-card.tsx
+++ src/components/aplication-card/aplication-card.tsx
@@ -9,65 +9,65 @@ import Text from '../text';
 import UserAvatar from '../user-avatar/user-avatar';
 import ServiceButton from '../service-button/service-button';
 
-interface IAplicationCard {
+interface IApplicationCard {
   id: string
   date?: string
   time?: string
   location?: string
   about?: string
-  complitedAppQuantity?: string
+  completedAppQuantity?: string
   userName: string
   userAvatar: string
   userPhone: string
 }
 
-const AplicationCard: FC<IAplicationCard> = ({
+const ApplicationCard: FC<IApplicationCard> = ({
   id,
   date,
   time,
   location,
   about,
-  complitedAppQuantity,
+  completedAppQuantity,
   userName,
   userAvatar,
   userPhone,
 }) => {
   return (
-    <div className={styles.aplicationCard} id={id}>
-      <div className={styles.aplicationCard__categoryBarContainer}>
+    <div className={styles.applicationCard} id={id}>
+      <div className={styles.applicationCard__categoryBarContainer}>
         <CategoryBar children={<Text children='категория' color={'#FFFFFF'}/>} bgColor={'#9798C9'}/>
       </div>
-      <div className={styles.aplicationCard__dateTimeLocation}>
-        <div className={styles.aplicationCard__dateContainer}>
+      <div className={styles.applicationCard__dateTimeLocation}>
+        <div className={styles.applicationCard__dateContainer}>
           <Calendar />
           <Text children={date} size={'24'} color={'#2E3192'} />
         </div>
-        <div className={styles.aplicationCard__dateContainer}>
+        <div className={styles.applicationCard__dateContainer}>
           <Clock />
           <Text children={time} size={'24'} color={'#2E3192'} />
         </div>
-        <div className={styles.aplicationCard__dateContainer}>
+        <div className={styles.applicationCard__dateContainer}>
           <LocationIcon />
           <Text children={location} size={'16'} color={'#2E3192'} />
         </div>
       </div>
-      <div className={styles.aplicationCard__about}>
+      <div className={styles.applicationCard__about}>
         <Text children={'Заголовок'} size={'24'} color={'#2E3192'} />
-        <div className={styles.aplicationCard__textContainer}>
-          <p className={styles.aplicationCard__textAbout}>{about}</p>
-          <p><a className={styles.aplicationCard__textAboutLink} href='*'>читать</a></p>
+        <div className={styles.applicationCard__textContainer}>
+          <p className={styles.applicationCard__textAbout}>{about}</p>
+          <p><a className={styles.applicationCard__textAboutLink} href='*'>читать</a></p>
         </div>
-        <div className={styles.aplicationCard__completedQuantity}>
+        <div className={styles.applicationCard__completedQuantity}>
           <ComplitedAplications width={'32px'} height={'32px'}/>
-          <Text children={complitedAppQuantity} color={'#2E3192'}/>
+          <Text children={completedAppQuantity} color={'#2E3192'}/>
         </div>
       </div>
-      <div className={styles.aplicationCard__userColumn}>
+      <div className={styles.applicationCard__userColumn}>
         <UserAvatar src={ userAvatar } />
         <Text children={userName} align={'center'} />
         <Text children={userPhone} />
       </div>
-      <div className={styles.aplicationCard__buttonsColumn}>
+      <div className={styles.applicationCard__buttonsColumn}>
         <ServiceButton />
         <ServiceButton viewType={'edit'}/>
       </div>
@@ -75,4 +75,4 @@ const AplicationCard: FC<IAplicationCard> = ({
   );
 };
 
-export default AplicationCard;
+export default ApplicationCard;
--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -3,50 +3,7 @@ import Footer from '../footer';
 import Box from '../box';
 import Text from '../text';
 import AplicationCard from '../aplication-card/aplication-card';
-
-const store = {
-  aplicationCardData: [
-    {
-      id: '1',
-      date: '24.10.2022',
-      time: '16:00',
-      lcoation: 'ул. Потолочного д. 9',
-      about: 'Описание заявки с возможностью развернуть и прочитать Описание заявки с возможностью развернуть и прочитать Описание заявки с  прочитать Описание заявки с',
-      complitedAppQuantity: '1',
-      owner: {
-        name: 'Петров Петр Петрович ',
-        avatar: 'https://i.pinimg.com/originals/53/1a/5c/531a5cf45c39bad3320af48e8ad8d324.png',
-        phone: '+7(000) 000-00-00',
-      },
-    },
-    {
-      id: '2',
-      date: '25.10.2022',
-      time: '13:00',
-      lcoation: 'ул. Дверная д. 13',
-      about: 'Описание заявки с возможностью развернуть и прочитать Описание заявки с возможностью развернуть и прочитать Описание заявки с  прочитать Описание заявки с',
-      complitedAppQuantity: '13',
-      owner: {
-        name: 'Алексеев Алексей Алексеевич ',
-        avatar: 'https://i.pinimg.com/originals/53/1a/5c/531a5cf45c39bad3320af48e8ad8d324.png',
-        phone: '+7(000) 000-00-13',
-      },
-    },
-    {
-      id: '3',
-      date: '13.10.2022',
-      time: '11:00',
-      lcoation: 'ул. Оконная д. 9',
-      about: 'Описание заявки с возможностью развернуть и прочитать Описание заявки с возможностью развернуть и прочитать Описание заявки с  прочитать Описание заявки с',
-      complitedAppQuantity: '33',
-      owner: {
-        name: 'Александров Александр Александрович',
-        avatar: 'https://i.pinimg.com/originals/53/1a/5c/531a5cf45c39bad3320af48e8ad8d324.png',
-        phone: '+7(000) 000-00-11',
-      },
-    },
-  ],
-};
+import { store } from '../aplication-card/application-card.constans';
 
 const aplicationCards = store.aplicationCardData
   .map(aplicationCard => <AplicationCard
@@ -54,9 +11,9 @@ const aplicationCards = store.aplicationCardData
     id={aplicationCard.id}
     date={aplicationCard.date}
     time={aplicationCard.time}
-    location={aplicationCard.lcoation}
+    location={aplicationCard.location}
     about={aplicationCard.about}
-    complitedAppQuantity={aplicationCard.complitedAppQuantity}
+    completedAppQuantity={aplicationCard.completedAppQuantity}
     userName={aplicationCard.owner.name}
     userAvatar={aplicationCard.owner.avatar}
     userPhone={aplicationCard.owner.phone}
