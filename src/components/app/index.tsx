d084d6b07dfa30d6ea345128a42c5c86dcf696c7
Ekaterina Kryuchkova <krekser37@yandex.ru>
2023-04-26 09:15:23 +0300

    add checkbox

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,5 +1,4 @@
 import styles from './app.module.scss';
-import Checkboxes from '../checkboxes/checkboxes';
 import Footer from '../footer';
 import Box from '../box';
 import Text from '../text';
@@ -7,29 +6,12 @@ import Text from '../text';
 function App() {
   return (
     <>
-      <div className={styles.app}>
-        <header className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className={styles.appLink}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-      <Checkboxes title='отображать' />
-      <div className={styles.app} />
-      <Box>
-        <Text tag='p' size='24' weight='700'>TEST</Text>
-      </Box>
-      <Footer />
-    </>
+    <div className={styles.app} />
+    <Box>
+      <Text tag='p' size='24' weight='700'>TEST</Text>
+    </Box>
+    <Footer />
+  </>
   );
 }
 
