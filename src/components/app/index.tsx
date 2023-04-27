c05a7e5fcf3d1daa83fde37ea8a6abaa37786c48
Ekaterina Kryuchkova <krekser37@yandex.ru>
2023-04-27 18:45:42 +0300

    feature: add modal

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,10 +1,25 @@
+/* import { useNavigate } from 'react-router-dom'; */
 import styles from './app.module.scss';
 import Footer from '../footer';
 import Box from '../box';
 import Text from '../text';
 import RadiusSearch from '../radius-search';
+import Modal from '../modal';
+import Button from '../button/button';
+import { useState } from 'react';
+import NavCards from '../nav-cards';
 
 function App() {
+  const [openPopup, setOpenPopup] = useState(false);
+  /*   const navigate = useNavigate(); */
+  /*   const handleCloseIngredientInModal = () => {
+    console.log(close);
+    navigate('/');
+  }; */
+  const handleClose = () => {
+    setOpenPopup(false);
+  };
+
   return (
     <>
       <div className={styles.app} />
@@ -12,6 +27,12 @@ function App() {
         <Text tag='p' size='24' weight='700'>TEST</Text>
       </Box>
       <RadiusSearch />
+      <Button viewType = 'primary' onClick={() => { setOpenPopup(true); }}>Открыть попап</Button>
+      {openPopup && <Modal
+            onClose={() => { handleClose(); }}
+          >
+            <NavCards></NavCards>
+      </Modal>}
       <Footer />
     </>
   );
---
+++ src/components/modal/index.tsx
@@ -0,0 +1,48 @@
+/* import React, { FC, ReactNode, useCallback, useEffect } from "react"; */
+import ReactDOM from 'react-dom';
+import { type FC, useCallback, useEffect } from 'react';
+import styles from './modal.module.scss';
+import ServiceButton from '../service-button/service-button';
+import ModalOverlay from '../modal-overlay';
+
+const modalRoot = document.getElementById('root') as HTMLElement;
+
+interface IModal {
+  title?: string
+  onClose: () => void
+  children?: React.ReactNode
+}
+
+const Modal: FC<IModal> = ({ onClose, children }) => {
+  const onEscDown = useCallback(
+    (e: { key: string }) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    },
+    [onClose],
+  );
+
+  useEffect(() => {
+    document.addEventListener('keydown', onEscDown);
+    return () => {
+      document.removeEventListener('keydown', onEscDown);
+    };
+  }, [onEscDown]);
+  return ReactDOM.createPortal(
+    <>
+      <div className={`${styles.modal}`}>
+        <div className={`${styles.modalHeader}`}>
+          <button className={`${styles.modalCloseButton}`}>
+            <ServiceButton viewType="close" onClick={onClose} />
+          </button>
+        </div>
+        {children}
+      </div>
+      <ModalOverlay onClose={onClose} />
+    </>,
+    modalRoot,
+  );
+};
+
+export default Modal;
