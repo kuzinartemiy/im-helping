bc8aed8d556dd1ce6b1632022bdb4ba6fb9a3e25
Ekaterina Kryuchkova <krekser37@yandex.ru>
2023-04-28 23:29:07 +0300

    add applicationСoordinates, icon location

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,6 +1,7 @@
 /* import * as React from 'react'; */
 import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 import styles from './map.module.scss';
+import location from '../../assets/icons/location.svg';
 /* import useGeolocation from 'react-hook-geolocation'; */
 
 const MapProd = () => {
@@ -8,6 +9,19 @@ const MapProd = () => {
     center: [55.751574, 37.573856],
     zoom: 10,
   };
+
+  const iconLayout = 'default#image';
+  const iconImageHref = location;
+  const iconImageSize = [53, 53];
+
+  /*  const coordinates = [55.751574, 37.573856]; */
+  const applicationСoordinates = [
+    [55.751574, 37.573856],
+    [55.61266, 37.684102],
+    [55.826296, 37.63765],
+    [55.701607, 37.531586],
+    [55.744561, 37.605463],
+  ];
 
   /*   const mapOptions = {
     modules: ['geocode', 'SuggestView'],
@@ -34,14 +48,19 @@ const MapProd = () => {
           width={'100%'}
           height={'728px'}
         >
-            <Placemark
-            /*   options={{
-                iconLayout: 'default#image',
-                iconImageHref: 'images/location.svg',
-                iconImageSize: [50, 50],
-              }} */
-              geometry={[55.751574, 37.573856]}
-            />
+          {applicationСoordinates.map((el, index) => {
+            return (
+              <Placemark
+                options={{
+                  iconLayout,
+                  iconImageHref,
+                  iconImageSize,
+                }}
+                geometry={el}
+                key={index}
+              />
+            );
+          })}
           {/*           {!geolocation.error && geolocation.latitude && (
               <Placemark
               className={styles.map_icon}
