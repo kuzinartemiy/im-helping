3bfa3f502505606a41a9decc8cbb84ee7e21f4e3
Ekaterina Kryuchkova <krekser37@yandex.ru>
2023-04-29 16:48:07 +0300

    for add in project

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,8 +1,7 @@
-/* import * as React from 'react'; */
+// import * as React from 'react';
 import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 import styles from './map.module.scss';
 import location from '../../assets/icons/location.svg';
-/* import useGeolocation from 'react-hook-geolocation'; */
 
 const MapProd = () => {
   const defaultState = {
@@ -14,7 +13,6 @@ const MapProd = () => {
   const iconImageHref = location;
   const iconImageSize = [53, 53];
 
-  /*  const coordinates = [55.751574, 37.573856]; */
   const applicationСoordinates = [
     [55.751574, 37.573856],
     [55.61266, 37.684102],
@@ -23,30 +21,22 @@ const MapProd = () => {
     [55.744561, 37.605463],
   ];
 
-  /*   const mapOptions = {
+  const mapOptions = {
     modules: ['geocode', 'SuggestView'],
     defaultOptions: { suppressMapOpenBlock: true },
-      width: '100%',
-    height: '728px',
-    defaultState: {
-      center: [55.751574, 37.573856],
-      zoom: 10,
-    },
-  }; */
-
-  /*   const geolocation = useGeolocation({
-    enableHighAccuracy: true,
-    maximumAge: 15000,
-    timeout: 12000,
-  }); */
+  };
 
   return (
     <div className={styles.map_container}>
       <YMaps>
         <Map
-          /* {...mapOptions} */ defaultState={defaultState}
-          width={'100%'}
+          {...mapOptions}
+          defaultState={defaultState}
           height={'728px'}
+          width={'100%'}
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          /* onLoad={async (instance) => instance.geocode('Саратов')}
+          modules={['geocode']} */
         >
           {applicationСoordinates.map((el, index) => {
             return (
@@ -61,16 +51,6 @@ const MapProd = () => {
               />
             );
           })}
-          {/*           {!geolocation.error && geolocation.latitude && (
-              <Placemark
-              className={styles.map_icon}
-                geometry={[geolocation.latitude, geolocation.longitude]}
-                options={{
-                  preset: 'islands#geolocationIcon',
-                  iconColor: 'green',
-                }}
-              />
-            )} */}
         </Map>
       </YMaps>
     </div>
