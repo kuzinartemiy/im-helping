f89cc117649b7a23baffe4cff1c58f2d2c4e5cd6
leondasferras <90497102+leondasferras@users.noreply.github.com>
2023-05-01 01:15:14 +0500

    add filter popup

---
+++ src/components/map/consts.js
@@ -0,0 +1,132 @@
+export const requests = [{
+  city: 'Москва',
+  requests: [
+    {
+      user: {
+        firstName: 'Ivan',
+        secondName: 'Ivanov',
+        phone: '8-800-555-35-35',
+      },
+      category: 'cat1',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, sapiente!',
+      coord: [55.751574, 37.573856],
+      qty: 7,
+    },
+    {
+      user: {
+        firstName: 'Ivan',
+        secondName: 'Ivanov',
+        phone: '8-800-555-35-35',
+      },
+      category: 'cat1',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, sapiente!',
+      coord: [55.61266, 37.684102],
+      qty: 7,
+    },
+    {
+      user: {
+        firstName: 'Ivan',
+        secondName: 'Ivanov',
+        phone: '8-800-555-35-35',
+      },
+      category: 'cat2',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, sapiente!',
+      coord: [55.826296, 37.63765],
+      qty: 7,
+    },
+    {
+      user: {
+        firstName: 'Ivan',
+        secondName: 'Ivanov',
+        phone: '8-800-555-35-35',
+      },
+      category: 'cat3',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, sapiente!',
+      coord: [55.701607, 37.531586],
+      qty: 7,
+    },
+    {
+      user: {
+        firstName: 'Ivan',
+        secondName: 'Ivanov',
+        phone: '8-800-555-35-35',
+      },
+      category: 'cat4',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, sapiente!',
+      coord: [55.744561, 37.605463],
+      qty: 7,
+    },
+  ],
+},
+
+{
+  city: 'Санкт-Петербург',
+  requests: [
+    {
+      user: {
+        firstName: 'Ivan',
+        secondName: 'Ivanov',
+        phone: '8-800-555-35-35',
+      },
+      category: 'cat1',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, sapiente!',
+      coord: [59.898513, 30.410314],
+      qty: 7,
+    },
+    {
+      user: {
+        firstName: 'Ivan',
+        secondName: 'Ivanov',
+        phone: '8-800-555-35-35',
+      },
+      category: 'cat1',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, sapiente!',
+      coord: [59.958874, 30.297918],
+      qty: 7,
+    },
+    {
+      user: {
+        firstName: 'Ivan',
+        secondName: 'Ivanov',
+        phone: '8-800-555-35-35',
+      },
+      category: 'cat2',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, sapiente!',
+      coord: [59.997818, 30.410777],
+      qty: 7,
+    },
+    {
+      user: {
+        firstName: 'Ivan',
+        secondName: 'Ivanov',
+        phone: '8-800-555-35-35',
+      },
+      category: 'cat3',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, sapiente!',
+      coord: [59.832920, 30.308094],
+      qty: 7,
+    },
+    {
+      user: {
+        firstName: 'Ivan',
+        secondName: 'Ivanov',
+        phone: '8-800-555-35-35',
+      },
+      category: 'cat4',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, sapiente!',
+      coord: [59.907342, 30.395513],
+      qty: 7,
+    },
+  ],
+},
+];
--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,33 +1,60 @@
-// import * as React from 'react';
-import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
+import { useState } from 'react';
+import { YMaps, Map, Placemark, Circle } from '@pbe/react-yandex-maps';
 import styles from './map.module.scss';
 import location from '../../assets/icons/location.svg';
+import TopPanel from '../top-panel';
+import { ReactComponent as MapIcon } from '../../assets/icons/requests-map.svg';
+import MapFilterPopup from '../map-filter-popup';
+import { CoordsPopup } from '../coords-popup/coords-popup';
+import { requests } from './consts';
+
+const defaultState = {
+  center: [55.751574, 37.573856],
+  zoom: 10,
+};
+
+const iconLayout = 'default#image';
+const iconImageHref = location;
+const iconImageSize = [53, 53];
+
+const applicationСoordinates = [
+  [55.751574, 37.573856],
+  [55.61266, 37.684102],
+  [55.826296, 37.63765],
+  [55.701607, 37.531586],
+  [55.744561, 37.605463],
+];
+
+const mapOptions = {
+  modules: ['geocode', 'SuggestView'],
+  defaultOptions: { suppressMapOpenBlock: true },
+};
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 const MapProd = () => {
-  const defaultState = {
-    center: [55.751574, 37.573856],
-    zoom: 10,
-  };
-
-  const iconLayout = 'default#image';
-  const iconImageHref = location;
-  const iconImageSize = [53, 53];
-
-  const applicationСoordinates = [
-    [55.751574, 37.573856],
-    [55.61266, 37.684102],
-    [55.826296, 37.63765],
-    [55.701607, 37.531586],
-    [55.744561, 37.605463],
-  ];
-
-  const mapOptions = {
-    modules: ['geocode', 'SuggestView'],
-    defaultOptions: { suppressMapOpenBlock: true },
-  };
+  const [btnCoords, setBtnCoords] = useState<any>();
+  const [currentCity, setCurrenCity] = useState('Москва');
+  const [searchOptions, setSearchOptions] = useState({
+    categories: [],
+    radius: 1,
+    time: new Date(),
+    date: new Date(),
+  });
+  const onFilterClick: React.MouseEventHandler = (e) => setBtnCoords([e.pageX, e.pageY]);
+  const onOverlayClick = () => setBtnCoords([]);
 
   return (
     <div className={styles.map_container}>
+      <TopPanel title='Карта' titleIcon={<MapIcon/>} onFilterClick={onFilterClick}/>
+      {btnCoords && btnCoords.length > 0 && <CoordsPopup pageX={btnCoords[0]} pageY={btnCoords[1]} onOverlayClick={onOverlayClick}><MapFilterPopup/></CoordsPopup>}
       <YMaps>
         <Map
           {...mapOptions}
@@ -38,6 +65,7 @@ const MapProd = () => {
           /* onLoad={async (instance) => instance.geocode('Саратов')}
           modules={['geocode']} */
         >
+          <Circle/>
           {applicationСoordinates.map((el, index) => {
             return (
               <Placemark
