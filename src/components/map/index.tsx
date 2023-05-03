e61c390866a314124f4d78a1d7d4d9cfd96f1bbb
Ekaterina Kryuchkova <krekser37@yandex.ru>
2023-05-03 15:02:15 +0300

    1

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,5 +1,5 @@
+
 import { useState, useEffect, useRef } from 'react';
-import { YMaps, Map, Placemark, useYMaps } from '@pbe/react-yandex-maps';
 import styles from './map.module.scss';
 import location from '../../assets/icons/location.svg';
 import TopPanel from '../top-panel';
@@ -7,112 +7,111 @@ import { ReactComponent as MapIcon } from '../../assets/icons/requests-map.svg';
 import MapFilterPopup from '../map-filter-popup';
 import { CoordsPopup } from '../coords-popup/coords-popup';
 import { requests } from './consts';
+import { /* YMaps, Map, Placemark,  */useYMaps } from '@pbe/react-yandex-maps';
+// import Select from '../select';
 
-const defaultState = {
-  center: [55.751574, 37.573856],
-  zoom: 10,
-};
+const API_KEY = '05f8d2ae-bd94-4329-b9f9-7351e2ec9627';
 
-const iconLayout = 'default#image';
-const iconImageHref = location;
-const iconImageSize = [53, 53];
-
-const applicationСoordinates = [
-  [55.751574, 37.573856],
-  [55.61266, 37.684102],
-  [55.826296, 37.63765],
-  [55.701607, 37.531586],
-  [55.744561, 37.605463],
-];
-
-const mapOptions = {
-  modules: ['geocode', 'SuggestView'],
-  defaultOptions: { suppressMapOpenBlock: true },
-};
-
-const API_KEY = "05f8d2ae-bd94-4329-b9f9-7351e2ec9627";
-//
-//
-//
-//
-//
-//
-//
-//
-//
+// 'geoObject.addon.balloon'
 
 const MapProd = () => {
-  const [options, setOptions] = useState<any>([]);
-  const [value, setValue] = useState("");
-  const mapRef = useRef();
-  const ymaps = useYMaps();
-  const [btnCoords, setBtnCoords] = useState<any>();
+  const [reqInfo, setReqinfo] = useState();
   const [currentCity, setCurrentCity] = useState<any>(requests[0]);
-  const [searchOptions, setSearchOptions] = useState({
-    categories: [],
-    radius: 1,
-    time: new Date(),
-    date: new Date(),
+  const [btnCoords, setBtnCoords] = useState<any>();
+  const [address, setAddress] = useState('Москва, ул. Льва Толстого, 16');
+  const [coordinates, setCoordinates] = useState([]);
+  let myMap: any;
+  const placemarkCollection: any = {};
+  const ymaps = useYMaps(['Map', 'GeoObjectCollection', 'Placemark', 'GeoObject',
+  ]);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if ((ymaps == null) || !mapRef.current || !currentCity) {
+      return;
+    }
+
+    myMap = new ymaps.Map(mapRef.current, {
+      center: [55.76, 37.64],
+      zoom: 10,
+    });
+
+    requests.forEach((city: any) => {
+      const cityCollection = new ymaps.GeoObjectCollection();
+      city.requests.forEach((req: any) => {
+        const reqPlaceMark = new ymaps.Placemark(
+          req.coord,
+          {
+            ...req,
+          },
+          {
+            iconLayout: 'default#image',
+            iconImageHref: location,
+            iconImageSize: [53, 53],
+          },
+        );
+        reqPlaceMark.events.add('click', function (e: any) {
+          setReqinfo(e.originalEvent.target.properties._data);
+        });
+        cityCollection.add(reqPlaceMark);
+        placemarkCollection[city.city.name] = cityCollection;
+      });
+      myMap.geoObjects.add(cityCollection);
+    });
+
+    
+  }, [ymaps]);
+
+  const  mySearchControl = new ymaps.control.SearchControl({
+    options: {
+      noPlacemark: true,
+    },
   });
+
   const onFilterClick: React.MouseEventHandler = (e) => setBtnCoords([e.pageX, e.pageY]);
   const onOverlayClick = () => setBtnCoords([]);
+  const onSelectChange = (e: any) => {
+    myMap?.setBounds(
+      placemarkCollection[e.target.value].getBounds(), { checkZoomRange: true },
+    )
+      .then(() => { if (myMap.getZoom > 15) myMap.getZoom(15); });
+  };
 
+  const handleAddressChange = (event) => {
+    setAddress(event.target.value);
+  };
+  const handleAddressSubmit = async(event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`https://geocode-maps.yandex.ru/1.x/?apikey=${API_KEY}&format=json&geocode=${address}`);
+      const data = await response.json();
+      const point = data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos;
+      const [longitude, latitude] = point.split(' ');
+      setCoordinates([parseFloat(latitude), parseFloat(longitude)]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-
-  // тут получаем по названию города его координаты и записываем в option
-  useEffect(() => {
-    (async () => {
-      try {
-        if (currentCity) {
-          const res = await fetch(
-            `https://geocode-maps.yandex.ru/1.x/?apikey=${API_KEY}&format=json&geocode=${currentCity.city.name}`
-          );
-          const data = await res.json();
-          const cityCoords = data.response.GeoObjectCollection.featureMember.map(
-            (item:any) => item.GeoObject,
-          );
-          setOptions(cityCoords);
-
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    })();
-  }, [currentCity]);
-  
   return (
-    <div className={styles.map_container}>
-      <TopPanel title='Карта' titleIcon={<MapIcon/>} onFilterClick={onFilterClick}/>
-      {btnCoords && btnCoords.length > 0 && <CoordsPopup pageX={btnCoords[0]} pageY={btnCoords[1]} onOverlayClick={onOverlayClick}><MapFilterPopup/></CoordsPopup>}
-
-        <select value={currentCity.city.name} onChange={e => setCurrentCity(requests.find(el => el.city.name === e.target.value))}>
+  <div className={styles.map_container}>
+    <TopPanel title='Карта' titleIcon={<MapIcon/>} onFilterClick={onFilterClick}/>
+    <select name={'Выберите город'} onChange={onSelectChange}>
           {requests.map((el, i) => <option value={el.city.name} key={i}>{el.city.name}</option>)}
-        </select>
-{       options[0]?.Point ?  <Map
-          {...mapOptions}
-          instanceRef={mapRef}
-          defaultState={{ center: options[0].Point.pos.split(" ").reverse(), zoom: 10 }}
-          height={'728px'}
-          width={'100%'}
-          modules={['geocode', 'GeoObjectCollection']}
-
-        >
-          {applicationСoordinates.map((el, index) => {
-            return (
-              <Placemark
-                options={{
-                  iconLayout,
-                  iconImageHref,
-                  iconImageSize,
-                }}
-                geometry={el}
-                key={index}
-              />
-            );
-          })}
-        </Map> : null}
-
-    </div>
+    </select>
+    <form onSubmit={handleAddressSubmit}>
+        <label>
+          Address:
+          <input type="text" value={address} onChange={handleAddressChange} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+{/*     <Select value={'Выберите город'} onChange={onSelectChange} elementsList={}>
+    {requests.map((el, i) => <option value={el.city.name} key={i}>{el.city.name}</option>)}
+    </Select> */}
+    {btnCoords && btnCoords.length > 0 && <CoordsPopup pageX={btnCoords[0]} pageY={btnCoords[1]} onOverlayClick={onOverlayClick}><MapFilterPopup/></CoordsPopup>}
+    <div style={{ height: '728px', width: '100%' }}ref={mapRef}></div>
+  </div>
   );
 };
 
