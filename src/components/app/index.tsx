a557bfec18668107d8a258e6f27e44839dbeee77
Artemiy <artemiy@MacBook-Air-Artemiy.local>
2023-05-03 22:50:59 +0500

    fix

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -19,7 +19,6 @@ import {
   VolunteerPage,
   RecipientPage,
 } from '../../pages';
-import DocumentsPopup from '../documents-popup/documents-popup';
 import CompletedFilterPopup from '../completed-filter-popup';
 import { YMaps } from '@pbe/react-yandex-maps';
 import { store } from '../../utils/application-card.constans';
