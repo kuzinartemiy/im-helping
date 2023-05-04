7bef82cf58a6c0587f1c1e3a5f1b5da263820e5d
Sergei Anokhin <sa.anokhin@yandex.ru>
2023-05-04 14:33:07 -0700

    Add AdminPage (html + scss)

--- src/pages/adminPage/adminPage.tsx
+++ src/pages/adminPage/adminPage.tsx
@@ -1,9 +1,39 @@
-function AdminPage() {
+import styles from './admin-page.module.scss';
+import { adminUsersData, adminData } from '../../utils/admin-default.constants';
+import AdminDefault from '../../components/admin-default';
+import TopPanel from '../../components/top-panel';
+import PersonVolunteer from '../../components/person-volunteer';
+import NavCards from '../../components/nav-cards';
+
+const AdminPage = () => {
   return (
-    <>
-      <p>Жду компонент администратора</p>
-    </>
+    <div className={styles.adminPage}>
+      <aside className={styles.adminSidebar}>
+        <PersonVolunteer avatar={adminData.avatar} name={adminData.name} id={adminData.id} phone={adminData.phone} about={adminData.about} />
+        <NavCards />
+      </aside>
+      <main className={styles.adminContent}>
+        <TopPanel title='Title' />
+        <div className={styles.userCards}>
+        {adminUsersData.map((userData, index) => (
+        <div className={styles.userCard}>
+        <AdminDefault
+          key={index}
+          userImg={userData.userImg}
+          userName={userData.userName}
+          userId={userData.userId}
+          userTel={userData.userTel}
+          userPoints={userData.userPoints}
+          userHelps={userData.userHelps}
+          userKey={userData.userKey}
+          lineColor={userData.lineColor}
+          confirmed={userData.confirmed}
+        /></div>
+        ))}
+        </div>
+      </main>
+    </div>
   );
-}
+};
 
 export default AdminPage;
