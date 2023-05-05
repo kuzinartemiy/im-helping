23079a5046075e3d023b289663efb06b59cec680
Daniil <kashel369@bk.ru>
2023-05-05 09:17:43 +0300

    static done

--- src/pages/superAdminPage/superAdminPage.tsx
+++ src/pages/superAdminPage/superAdminPage.tsx
@@ -1,8 +1,20 @@
+import NavCards from '../../components/nav-cards';
+import PersonVolunteer from '../../components/person-volunteer';
+import TopPanel from '../../components/top-panel';
+import { ApprovedOrBlockSvg, superAdminTestPersonProps } from './superAdminPage.constants';
+import style from './superAdminPage.module.scss';
+
 function SuperAdminPage() {
   return (
-    <>
-      <p>Жду компонент главного администратора</p>
-    </>
+    <div className={style.wrapper}>
+      <div className={style.leftSide}>
+        <PersonVolunteer {...superAdminTestPersonProps} />
+        <NavCards />
+      </div>
+      <div className={style.content}>
+        <TopPanel title="Подтверждение / Блокировка" titleIcon={<ApprovedOrBlockSvg />} />
+      </div>
+    </div>
   );
 }
 
