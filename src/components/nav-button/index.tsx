f285ca2dfcc080c92acb41d68ab1b437f5329606
Ekaterina Kryuchkova <krekser37@yandex.ru>
2023-05-06 19:40:26 +0300

    add NavButton

--- src/components/nav-button/index.tsx
+++ src/components/nav-button/index.tsx
@@ -1,17 +1,20 @@
 import { type ButtonHTMLAttributes, type DetailedHTMLProps } from 'react';
-import { ReactComponent as ArrowIcon } from '../../assets/icons/arrow-down.svg';
+/* import { ReactComponent as ArrowIcon } from '../../assets/icons/arrow-down.svg'; */
+import { ReactComponent as Search } from '../../assets/icons/search.svg';
 import Text from '../text';
 import styles from './nav-button.module.scss';
 import { COLORS } from '../../styles/colors';
 
 interface INavButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
   children?: React.ReactNode
+  isDisabled?: boolean
 }
 
-const NavButton = ({ children, ...props }: INavButtonProps) => {
+const NavButton = ({ children, isDisabled, ...props }: INavButtonProps) => {
   return (
-    <button className={styles.navButton} {...props}>
-      <ArrowIcon fill={COLORS.get('background')} />
+    <button disabled={isDisabled} className={styles.navButton} {...props}>
+     {/*  <ArrowIcon fill={COLORS.get('background')} /> */}
+      <Search />
       <Text lineHeight='19px' size='16' color={COLORS.get('background')}>{children}</Text>
     </button>
   );
